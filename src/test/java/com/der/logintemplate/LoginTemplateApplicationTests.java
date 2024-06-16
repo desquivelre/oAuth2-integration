fd50a7f4537b035abf02ebc614256b07914b89db
Diego Esquivel <desquivelreuni@hotmail.com>
2024-06-15 20:37:19 -0500

    Add new project and MySQL JDBC

---
+++ src/test/java/com/der/logintemplate/LoginTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.der.logintemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
